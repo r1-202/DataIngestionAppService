b42fff5e99dc1ccaeb3419ca51fcf1f954c55abf
Kassem Hmady <khmkhm626@gmail.com>
2024-11-26 18:58:04 +0200

    Modified instance variables to better align with the required task.

--- WeatherData.cs
+++ WeatherData.cs
@@ -1,33 +1,63 @@
 using System.Text.Json.Nodes;
 
+public class Units
+{
+    public Units(string time, string temperature, string pressure,
+    string humidity, string clouds, string wind_speed, string wind_direction)
+    {
+        this.time = time;
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.clouds = clouds;
+        this.wind_speed = wind_speed;
+        this.wind_direction = wind_direction;
+    }
+    string time{get;set;}
+    string temperature{get;set;}
+    string pressure{get;set;}
+    string humidity{get;set;}
+    string clouds{get;set;}
+    string wind_speed{get;set;}
+    string wind_direction{get;set;}
+}
+
 public class WeatherData
 {
-    public WeatherData(string city_name, string country_ID, 
-    string weather_description, float temperature)
+    //city is fixed to 33.8485° N, 35.8981° E (Zahle)
+    public WeatherData(string time, float temperature, float pressure,
+                        float humidity, float clouds, float wind_speed,
+                        float wind_direction, string season)
     {
-        this.city_name=city_name;
-        this.country_ID=country_ID;
-        this.weather_description=weather_description;
-        this.temperature=temperature;
+        this.time = time;
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.clouds = clouds;
+        this.wind_speed = wind_speed;
+        this.wind_direction = wind_direction;
     }
-    public string city_name{get;set;}
-    public string country_ID{get;set;}
-    public string weather_description{get;set;}
+    public string time{get;set;}
     public float temperature{get;set;}
+    public float pressure{get;set;}
+    public float humidity{get;set;}
+    public float clouds{get;set;}
+    public float wind_speed{get;set;}
+    public float wind_direction{get;set;}
+    public string season{get;set;}
+    public Units units;
 
-    public static async Task<WeatherData?> requestData(string city_name, string API_KEY)
+
+    public static async Task<List<WeatherData>?> requestData(string start_d, string end_d)
     {
-        string country_ID="";
-        string weather_description="";
-        float temperature=0;
-
         HttpClient client = new HttpClient();
-        client.BaseAddress = new Uri("http://api.openweathermap.org");
+        client.BaseAddress = new Uri("https://archive-api.open-meteo.com");
         var response = await client.GetAsync(
-            $"/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric");
+            $"/v1/archive?latitude=33.8485&longitude=35.8981"+
+            $"&start_date={start_d}&end_date={end_d}&"+
+            "hourly=temperature_2m,relative_humidity_2m,surface_pressure,"+
+            "cloud_cover,wind_speed_10m,wind_direction_10m");
         string json_response = await response.Content.ReadAsStringAsync();
-
-
 
         if(json_response==null) return null;
 
